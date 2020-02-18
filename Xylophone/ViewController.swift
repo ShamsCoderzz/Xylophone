a2511404f21e9b1eabe678153fc312d42f875e68
muhammadshams <mr.shamssheikh@gmail.com>
2020-02-19 01:14:50 +0500

    final code with delay

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,13 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var bombSoundEffect: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
-
+    
+    
+    @IBAction func playSound(_ sender: UIButton) {
+        // also sender.currentTitle
+        // will return name like C , A , B ,E
+        
+      //  perform(#selector(authenticate), with: nil, afterDelay: 1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.selectSound(soundName: (self.randomString())+".wav")
+            
+         //   print(self.randomString())
+        }
+    }
+    
+   
+    
+    
+    
+    func selectSound(soundName: String){
+        let path = Bundle.main.path(forResource: soundName, ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    
+    func randomString() -> String {
+      let arry = ["A","B","C","D","E","F","G"]
+        
+        let random = Int.random(in: 0..<arry.count)
+        return arry[random]
+    }
 
     
 
